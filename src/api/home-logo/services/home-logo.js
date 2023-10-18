5854f7119bd455e957a830370bf9e83429b552de
Selmin Lekovic <selminlekovic@gmail.com>
2023-10-18 17:57:56 +0200

    test

---
+++ src/api/home-logo/services/home-logo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-logo service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-logo.home-logo');
